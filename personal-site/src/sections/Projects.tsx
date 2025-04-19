e851ba8788b14168c5e5c54732a4c229cbfc1f6b
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-04-19 15:52:40 -0400

    -Created Projects section

--- personal-site/src/sections/Projects.tsx
+++ personal-site/src/sections/Projects.tsx
@@ -1,10 +1,22 @@
 "use client";
+import { RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
 
 export default function Projects() {
     return (
-    <section className  = "mt-20 px-50">
-        <h1>Projects</h1>
+    <section className  = "mt-20 flex-col justify-center">
+        <h1 className = "text-center text-3xl sm:text-5xl font-medium">Projects</h1>
 
+        <div className = "flex justify-between">
+            <button className = "justify-start text-6xl pl-10">
+                <RxDoubleArrowLeft/>
+            </button>
+            <div>
+                
+            </div>
+            <button className = "justify-end text-6xl pr-10">
+                <RxDoubleArrowRight/>
+            </button>
+        </div>
     </section>
     );
 }
