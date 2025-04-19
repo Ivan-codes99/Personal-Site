f72ae949fa31d9bfedd04dab008f4560c299b83b
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-04-19 13:18:46 -0400

    Created Projects section

--- personal-site/src/app/page.tsx
+++ personal-site/src/app/page.tsx
@@ -1,12 +1,13 @@
 
 import AboutMe from "@/sections/AboutMe";
 import Hero from "@/sections/Hero";
-
+import Projects from "@/sections/Projects";
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutMe/>
+      <Projects/>
     </>
   );
 }
---
+++ personal-site/src/sections/Projects.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Projects() {
+    return (
+    <section className  = "mt-20 px-50">
+        <h1>Projects</h1>
+
+    </section>
+    );
+}
