5600d9da8b0a18a8d8d012e7790e54679d800840
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-04-25 20:32:26 -0400

    removed unused variables

--- personal-site/src/sections/Projects.tsx
+++ personal-site/src/sections/Projects.tsx
@@ -2,7 +2,6 @@
 
 import projects from "../lib/project_data";
 import ProjectSlide from "@/components/ProjectSlide";
-import { RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
 
 export default function ProjectsSection() {
     return (
