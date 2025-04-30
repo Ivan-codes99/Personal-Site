df9694c23f4d1ef1c020168dd458ef1cdf0f3ed6
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-04-30 18:52:19 -0400

    Added experience data and added images

---
+++ personal-site/src/lib/experience_data.ts
@@ -0,0 +1,33 @@
+const experience = [
+    {
+      title: "Project Coordinator",
+      organization: "AI4ALL",
+      date: "Aug. 2024 - Dec. 2024",
+      dateSort: new Date("2024-12-01"), // Use end date for sorting
+      description:
+        "I mentored students on machine learning projects, helping them with Python debugging, data preprocessing, and applying techniques like regression and feature selection. I also coordinated technical tasks to support successful project outcomes.",
+      media: "/assets/AI4ALL.jpg",
+      tags: ["Python", "Machine learning", "Remote Coordination", "Project Management", "Leadership"],
+    },
+    {
+      title: "Guild Mentor",
+      organization: "Break Through Tech",
+      date: "Dec. 2023",
+      dateSort: new Date("2023-12-01"),
+      description:
+        "Served as a mentor for Break Through Tech Miami Winter Guild program, guiding a high school student team in designing and building a mobile app prototype for study group matching, all within a 5 day sprint.",
+      media: "/assets/Break_through_tech.png",
+      tags: ["Mentorship", "Leadership", "Prototyping"],
+    }, 
+    {
+      title: "IT Intern",
+      organization: "Memorial Healthcare System",
+      date: "May. 2023 - Jul. 2023",
+      dateSort: new Date("2023-07-01"),
+      description:
+        "Wrote Python scripts to assist with data migration during the transition from Lawson to Workday. Focused on parsing and formatting CSV invoice data, reducing manual entry and supporting cleaner system handoffs.",
+      media: "/assets/mhs.jpg",
+      tags: ["Python", "CSV parsing", "Automation", "Scripting"],
+    },
+  ];
+  
